7c6a2d850a6c1c74cb1e185fb89bb3c2fb8cc20c
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-07 12:50:04 +0100

    solution to assessment7

---
+++ Assessments/assessment7.py
@@ -0,0 +1,6 @@
+# Question 4: Write a function that takes a string as input and returns a new string that is the reverse of the input string.
+def reverse_string(string):
+    return string[::-1]
+
+
+print(reverse_string('string'))

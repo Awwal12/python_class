a1438f1e0a6b3e80cdb4373db5b99d1dc9162cc5
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-15 10:47:23 +0100

    solution to assessment 2

--- In-Class_Assessment/14th_June/assessment2.py
+++ In-Class_Assessment/14th_June/assessment2.py
@@ -1,1 +1,14 @@
-# write a program that takes in a list of words as input and returns a new list that contains only words that has more than 4 or more characters.
+# write a program that takes in a list of words as input and returns a new list that contains only words that has more than 6 or more characters.
+words = ["Airline",	"Gigantic", "Publish", "Bandit", "Goofy", "Quadrangle",
+         "Banquet", "Government"]
+
+
+def words_filter(words):
+    new_words = []
+    for x in words:
+        if len(x) > 6:
+            new_words.append(x)
+    return new_words
+
+
+print(words_filter(words))

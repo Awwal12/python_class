84be9417d84fc597d1076b8fa6b7cc640686a8c2
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-15 10:17:27 +0100

    solution to assessment 1

--- In-Class_Assessment/14th_June/assessment1.py
+++ In-Class_Assessment/14th_June/assessment1.py
@@ -1,1 +1,11 @@
 # write a function called "multiply_even_numbers" that takes in a list of numbers as parameters and returns the product of all even numbers, if there is no even numbers return 1.
+def multiply_even_numbers(numbers):
+    val = 1
+    for x in numbers:
+        if x % 2 == 0:
+            val *= x
+    return val
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+print(multiply_even_numbers(numbers))

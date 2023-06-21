7cd135cb58a9f938a0687422ea9c5cf65ef8ae61
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-21 11:47:26 +0100

    Question

---
+++ In-Class_Assessment/21th_june/classWork.py
@@ -0,0 +1,4 @@
+# Define a class called Employee with a construtor that takes a name and a salary...  The class should have a method called get_salary that returns the salary of the employee
+# Define two subclasses of Employee called Manager and Engineer, that inherit from Employee and have their own bonus attribute .... The manager class should hve a bonus of 10% of the salary and the Engineer class should have a bonus of 5% of the salary... The subclasses should override the get_salary method to return the salary plus the bonus.
+# Write a function called payroll that takes a list of employees and prints thier name and salary .
+# Create a list of employees with different names and salaries and pass it to the payroll function.

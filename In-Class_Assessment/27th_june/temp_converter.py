ee9e705369482ff5f91eab1f2c1ac6c33b29825a
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-27 12:46:37 +0100

    Question

---
+++ In-Class_Assessment/27th_june/temp_converter.py
@@ -0,0 +1,1 @@
+# Defined a function called kelvienToFaherenheit to convert temperature from kelvin to fahrenheit. The function should have an assert that check if the temperature is greater or equal to zero, if not display a message 'Colder that absolute zero' Formula =(k-273.15)*9/5 + 32

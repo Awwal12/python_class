253a16a96a4b164b5fb4a51642e5d15c009655a0
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-06-30 18:15:02 +0100

    Queston

---
+++ Assessments/30th_june/squared.py
@@ -0,0 +1,5 @@
+# You are tasked with writing a program that asks the user to input a number and prints out the result of the number squared. However, the program should also handle the following exceptions:
+
+# If the user inputs a non-numeric value, the program should print an error message and ask the user to input a valid number.
+# If the user inputs a negative number, the program should print an error message and ask the user to input a positive number.
+# Write a Python program that implements the above requirements. Your program should use the try and except statements to catch and handle the exceptions. The program should keep asking the user for input until a valid number is provided, and then print out the squared result.

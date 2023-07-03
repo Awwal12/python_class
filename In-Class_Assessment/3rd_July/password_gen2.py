bd89f05a1cd29108a6dfd5f6df73a5defc4cfce3
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-03 11:35:02 +0100

    advance generator

---
+++ In-Class_Assessment/3rd_July/password_gen2.py
@@ -0,0 +1,18 @@
+import random as r
+
+
+def generatePassword(length):
+    lower_case = 'abcdefghijklmnopqrstuvwxyz'
+    upper_case = lower_case.upper()
+    special_characters = '!"@#$%^&*()_+?><~`'
+
+    password = []
+    for _ in range(length):
+        our_choice = [r.choice(special_characters), str(
+            r.randrange(0, 9)), r.choice(lower_case), r.choice(upper_case)]
+        password.append(r.choice(our_choice))
+    return ''.join(password)
+
+
+pss = generatePassword(2)
+print(pss)

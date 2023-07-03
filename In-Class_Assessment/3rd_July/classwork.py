76f37968bfe76b24edf89a9fceb2562113ad0e26
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-03 12:01:22 +0100

    Question

---
+++ In-Class_Assessment/3rd_July/classwork.py
@@ -0,0 +1,13 @@
+# Define a class Student with the following attribute;
+# name(String)
+# age(integer)
+# score (integer)
+# 2. Create a file (name.txt) and add 10 name in each line
+# 3. Use the open function to read from the file and get the names
+# 4. Use the names gotten from the file to create a student object, using the names as attribute name, use the random package to create random ages from 16 – 19, and use the random package to add random scores from 20, 100
+# 5. Create a method in Student to get the grade of the score;
+# < 30 = F
+# 30 – 40 = E
+# 40 – 60 = C
+# 60 – 70 = B
+# 70  - 100= A

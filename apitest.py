ffc724917f6bb66e8f66e4a3c195fb1705f07136
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-10 14:03:05 +0100

    update

--- apitest.py
+++ apitest.py
@@ -1,9 +1,17 @@
 import requests
-url = 'https://api.publicapis.org/entries'
-response = requests.get(url)
-content = response.json()
-print(content['entries'][0]["Description"])
+# url = 'https://api.publicapis.org/entries'
+# response = requests.get(url)
+# content = response.json()
+# print(content['entries'][0]["Description"])
 
-resp = requests.put(
-    url, {'userId': 20, 'title': 'Do assessment', 'data': None})
+# resp = requests.put(
+#     url, {'userId': 20, 'title': 'Do assessment', 'data': None})
+# print(resp.json())
+
+url = 'https://api-customer-038j.onrender.com/customer/'
+response = requests.put(
+    url, {'name': 'Whos Jake', 'age': 66, 'order': 'jollof rice'})
+resp = requests.get(url)
+
+# print(response.json())
 print(resp.json())

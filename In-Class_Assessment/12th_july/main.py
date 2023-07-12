bd9f508bd6cb27bc8a32097e3a6ee721aa248220
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-12 12:51:49 +0100

    DOne

--- In-Class_Assessment/12th_july/main.py
+++ In-Class_Assessment/12th_july/main.py
@@ -1,1 +1,12 @@
 # scrap a movie website and get the 'trendings' list
+import requests
+from bs4 import BeautifulSoup as BS
+URL = "https://goku.sx/home"
+
+response = requests.get(URL)
+contents = BS(response.text, 'html.parser')
+container = contents.find('div', class_='tab-pane show active')
+items = container.find_all('div', class_='item')
+for item in items:
+    name = item.find('h3', class_='movie-name')
+    print(name.text)

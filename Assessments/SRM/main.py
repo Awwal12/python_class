a1816f36044290ec792ab582d8a6bfcb5564f98a
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-07-24 10:49:11 +0100

    final update

--- Assessments/SRM/main.py
+++ Assessments/SRM/main.py
@@ -1,8 +1,8 @@
-class DuplicateStudentIDError:
+class DuplicateStudentIDError(UserWarning):
     pass
 
 
-class StudentNotFoundError:
+class StudentNotFoundError(UserWarning):
     pass
 
 
@@ -12,18 +12,37 @@ class Student:
         self.name = name
         self.age = age
 
+    def format_data(self):
+        return f'{self.id} {self.name} {self.age}'
+
 
 class StudentRecordManager:
     def add_student(self, student: Student) -> None:
-        with open("student_records.txt", "r+") as f:
-            for x in f:
-                if x == student:
-                    raise Exception("DuplicateStudentIDError")
-                else:
-                    f.writelines()
+        with open("student_records.txt", "w+") as file:
+            for text in file.readlines():
+                if text == student:
+                    raise DuplicateStudentIDError
+                print('here')
+            file.write(student + "\n")
 
-    def get_student(self, id: int) -> Student:
-        pass  # Retrieves a student record based on the given ID
+    def get_student(self, id):
+        with open("student_records.txt", "r+", encoding='utf-8') as file:
+            for text in file.readlines():
+                if str(id) == text.split(' ')[0]:
+                    return text.strip('\n')
+            raise StudentNotFoundError
 
     def update_student(self, student: Student) -> None:
-        pass  # Updates an existing student record in the file
+        with open("student_records.txt", "a", encoding="utf-8") as file:
+            for text in file.readlines():
+                if str(student.split(' ')[0]) == text.split(' ')[0]:
+                    file.writelines(student + "\n")
+                else:
+                    raise StudentNotFoundError
+
+
+manager = StudentRecordManager()
+
+student1 = Student(1, "Moh", 33)
+manager.add_student(student1.format_data())
+print(manager.get_student(1))

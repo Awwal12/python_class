8a53385f19b722be5ff122d13c449fc6ad6c4ffa
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-08-07 16:30:24 +0100

    foundation

---
+++ Assessments/Online_shopping/app.py
@@ -0,0 +1,46 @@
+import mysql.connector as sql
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+user = os.environ.get('user')
+password = os.environ.get('password')
+host = os.environ.get('host')
+
+
+class DB:
+    def __init__(self):
+        self.__db = sql.connect(
+            host=host,
+            password=password,
+            user=user,
+            database='supermart'
+        )
+        self.productT = 'product'
+        self.customerT = 'customer'
+        self.orderT = 'orders'
+        self.orderItem = 'order_item'
+        self.__cursor = self.__db.cursor()
+
+
+class Product:
+    def __init__(self, product_id, name, category, price, stock_quantity):
+        self.product_id = product_id
+        self.name = name
+        self.category = category
+        self.price = price
+        self.stock_quantity = stock_quantity
+
+
+class Customer:
+    def __init__(self, customer_id, name, email, address):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.address = address
+
+
+class Order:
+    def __init__(self) -> None:
+        pass
